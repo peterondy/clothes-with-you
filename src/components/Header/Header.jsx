cae8f8345fb432d6e4e0724551c82fa7bbdb0def
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2023-12-31 23:29:43 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './header.css'
 const Header = () => {
   return (
     <header className='h-28 w-full'>
-        <a href='/'><h1 className='text-5xl pt-8 hover:text-gray-800'>Clothes With You</h1></a>
+        <a href='/'><h1 className='text-5xl pt-8 text-[#aaa69a] hover:text-[#5f6363]'>Clothes With You</h1></a>
     </header>
   )
 }
