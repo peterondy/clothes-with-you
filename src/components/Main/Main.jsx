4ebf58592ec72a1a17182fd878b3634b308a86b6
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-01-01 23:29:16 +0100

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,58 +1,34 @@
-h3{
-    font-family: 'Ruda', sans-serif;
-    font-family: 'Staatliches', sans-serif;
-}
-main .w-full ul{
-    margin-left: 500px;
-    width: 600px;
-    margin-top: 8rem;
-}
-main .w-full ul li{
-    margin-top: .5rem;
-    margin-bottom: .5rem;
-    padding-top: .5rem;
-    padding-bottom: .5rem;
+import React from 'react'
+//import background from '../../images/video_background.mp4'
+import './main.css'
+import walpaper from '../../images/walpaper.jpg'
+import logo from '../../images/logo.png'
 
+const Main = () => {
 
+    return (
+        <main className='h-screen w-full'
+            style={{ 
+                height: 'calc(100vh - 112px)', 
+                backgroundImage: `url(${walpaper})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundAttachment: 'fixed'
+            }}>
+            <h2 className='text-[#ffffff] text-5xl pt-8 z-50 inline-flex uppercase'><b>clothes with you</b> <img src={logo} className='w-[100px] h-[80px] relative top-[-30px]' alt="logo" /></h2>
+            <div className='w-full'>
+                <h3 className='text-[#c4c2c5] text-2xl bg-transparent'><b>Welcome in the complex styling's world</b></h3>
+            </div>
+            <div className='w-full z-50 text-center justify-center justify-items-center justify-self-center'>
+                <ul>
+                    <li className='w-full border-gray-50 border-solid border-[1px] text-3xl'><a href="/outfit" className='text-gray-50 my-8 rounded w-full hover:text-gray-400'>I'm looking for outfit</a></li>
+                    <li className='w-full border-gray-50 border-solid border-[1px] text-3xl'><a href="/piece" className='text-gray-50 my-8 rounded w-full hover:text-gray-400'>I'm looking for a piece of clothing</a></li>
+                    <li className='w-full border-gray-50 border-solid border-[1px] text-3xl'><a href="/ai-power" className='text-gray-50 my-8 rounded w-full hover:text-gray-400'>I discover AI power</a></li>
+                </ul>
+            </div>
+        </main>
+    )
 }
 
-@media (max-width: 1280px) {
-    main .w-full ul{
-        margin-left: 300px;
-        margin-top: 8rem;
-    }
-}
-@media (max-width: 1024px) {
-    main .w-full ul{
-        margin-left: 200px;
-        margin-top: 1.5rem;
-    }
-}
-@media (max-width: 820px) {
-    main .w-full ul{
-        margin-left: 130px;
-    }
-}
-@media (max-width: 600px) {
-    main .w-full ul{
-        margin-left: 70px;
-        margin-top: 1.5rem;
-        width: 400px;
-    }
-}
-@media (max-width: 500px) {
-    main .w-full ul{
-        margin-left: 10px;
-        margin-top: 1.5rem;
-        width: 380px;
-    }
-}
-@media (max-width: 300px) {
-    main .w-full ul{
-        width: 250px;
-    }
-    main .w-full ul li{
-        padding-top: 0.5rem;
-        padding-bottom: 0.5rem;
-    }
-}
+export default Main
