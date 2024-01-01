51d415548604cdaf686d4b9f2487e83f8a24e747
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-01-01 11:05:29 +0100

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,14 +4,12 @@ import './main.css'
 
 const Main = () => {
     return (
-        <main className='h-screen w-full'>
-            <div className="video-background">
-                <video autoPlay loop muted poster="poster.jpg">
-                    <source src={background} type="video/mp4" />
-                </video>
-            </div>
-            <h2 className='text-[#8f8f80] text-3xl pt-8'>enter into the complex styling world with AI technology</h2>
-            <div className='w-full'>
+        <main className='h-screen w-full video-background'>
+            <video autoPlay loop muted poster="poster.jpg" className='z-[-999]'>
+                <source src={background} type="video/mp4" />
+            </video>
+            <h2 className='text-[#8f8f80] text-3xl pt-8 z-50'>enter into the complex styling world with AI technology</h2>
+            <div className='w-full z-50'>
                 <div className='inline-flex mt-24 text-4xl space-x-4 text-gray-700 w-full justify-center'>
                     <a href="/signup" className='hover:text-gray-500 rounded py-2 px-4 bg-[#5f6363] text-[#1a2843]'>sign up</a>
                     
