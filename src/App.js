e962508353bcb5b0f8f6b4e82ffceec9e7157a55
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-01-01 23:01:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Command from './components/Command/Command'
 import Discover from './components/Discover/Discover'
-
+import walpaper from './images/walpaper.jpg'
 function App() {
   return (
-    <div className="App">
+    <div className="App"
+      style={{ 
+        height: 'calc(100vh - 112px)', 
+        backgroundImage: `url(${walpaper})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed'
+    }}>
       <Header/>
       <Main/>
-      <Command/>
-      <Discover/>
     </div>
   );
 }
