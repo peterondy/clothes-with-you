5c38281cb2da14e47ad59ef9ba242bcb57ffb53b
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-01-01 23:06:40 +0100

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import background from '../../images/video_background.mp4'
 import './main.css'
+import walpaper from '../../images/walpaper.jpg'
+import logo from '../../images/logo.png'
 
 const Main = () => {
+
     return (
-        <main className='h-screen w-full video-background'>
-            <video autoPlay loop muted poster="poster.jpg" className='z-[-999]'>
-                <source src={background} type="video/mp4" />
-            </video>
-            <h2 className='text-[#8f8f80] text-3xl pt-8 z-50'>enter into the complex styling world with AI technology</h2>
-            <div className='w-full z-50'>
-                <div className='inline-flex mt-24 text-4xl space-x-4 text-gray-700 w-full justify-center'>
-                    <a href="/signup" className='hover:text-gray-500 rounded py-2 px-4 bg-[#5f6363] text-[#1a2843]'>sign up</a>
-                    
-                    <a href="/login" className='hover:text-gray-500 rounded py-2 px-4 text-[#e9e8e3]'>log in</a>
-                </div>
-                <div className='mt-2 w-full'><p className='text-[#e9e8e3]'>Try for free</p></div>
-                <div className='w-full'>
-                    <h3 className='mt-28 text-[#aaa69a] text-2xl bg-transparent'><b>what do you want to dress you today ?</b></h3>
-                </div>
+        <main className='h-screen w-full'
+            style={{ 
+                height: 'calc(100vh - 112px)', 
+                backgroundImage: `url(${walpaper})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundAttachment: 'fixed'
+            }}>
+            <h2 className='text-[#ffffff] text-5xl pt-8 z-50 inline-flex uppercase'><b>clothes with you</b> <img src={logo} className='w-[100px] h-[80px] relative top-[-30px]' alt="logo" /></h2>
+            <div className='w-full'>
+                <h3 className='text-[#c4c2c5] text-2xl bg-transparent'><b>Welcome in the complex styling's world</b></h3>
+            </div>
+            <div className='w-full z-50 text-center justify-center justify-items-center justify-self-center'>
+                <ul>
+                    <li className='w-full border-gray-50 border-solid border-[1px] text-3xl'><a href="/outfit" className='text-gray-50 my-8 rounded w-full hover:text-gray-400'>I'm looking for outfit</a></li>
+                    <li className='w-full border-gray-50 border-solid border-[1px] text-3xl'><a href="/piece" className='text-gray-50 my-8 rounded w-full hover:text-gray-400'>I'm looking for a piece of clothing</a></li>
+                    <li className='w-full border-gray-50 border-solid border-[1px] text-3xl'><a href="/ai-power" className='text-gray-50 my-8 rounded w-full hover:text-gray-400'>I discover AI power</a></li>
+                </ul>
             </div>
         </main>
     )
