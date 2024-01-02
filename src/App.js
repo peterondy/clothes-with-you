26573a70740ba80d996021fa822b13e56d0c1b79
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-01-02 19:10:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import './App.css'
-import Header from './components/Header/Header'
 import Main from './components/Main/Main'
-import walpaper from './images/walpaper.jpg'
 function App() {
   return (
-    <div className="App"
-      style={{ 
-        height: 'calc(100vh - 112px)', 
-        backgroundImage: `url(${walpaper})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundAttachment: 'fixed'
-    }}>
-      <Header/>
+    <div className="App" style={{ height: '100vh', backgroundColor: '#000000'}}>
       <Main/>
     </div>
   );
