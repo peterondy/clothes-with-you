c785146c6480d0b252523fc37ec1ab050fcbcf2e
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-01-03 19:07:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
 import Main from './components/Main/Main'
+import Discover from './components/Discover/Discover'
+
 function App() {
   return (
-    <div className="App" style={{ height: '100vh', backgroundColor: '#000000'}}>
+    <div className="App">
       <Main/>
+      <Discover/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
