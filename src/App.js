5a211a3fc53e455c836092c73631ff39f7eee96a
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-01-04 06:12:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css'
 import Main from './components/Main/Main'
 import Discover from './components/Discover/Discover'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Main/>
-      <Discover/>
+      <BrowserRouter>
+        <Routes>
+            <Route exact path="/" Component={Main}/>
+            <Route exact path="/discover-ai" Component={Discover}/>
+          </Routes>
+      </BrowserRouter>
     </div>
   )
 }
