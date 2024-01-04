71be37dd033dcdf7fd6eeab0814e9a4804c7500b
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-01-04 06:13:36 +0100

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
             <header className='h-28 w-full'>
                 <h1 className='text-2xl pt-8 text-[#ffffff] w-full' style={{fontFamily: 'Staatliches'}}> <span className='float-left float-start mr-4'>Developed by SEN</span></h1>
             </header>
-            <main className='h-full w-full'>
+            <main className='w-full'>
                 <div className="w-full inline-flex">
                     <h2 className='float-left float-start text-[#ffffff] w-full z-50 inline-flex uppercase'><b><span className='float-left'>clothes</span> <br />  <span className='float-left float-start'>with <br /> you</span> <img src={logo} alt="logo" /></b></h2>
                 </div>
@@ -17,7 +17,7 @@ const Main = () => {
                     <h3 className='text-[#ffffff] bg-transparent w-full element'><b className='float-left'><span className='float-left'>Welcome into the complex </span><br /> <span className='float-left'> styling's world</span></b></h3>
                 </div>
                 <div className='w-full'>
-                    <a href="/discover">Get started</a>
+                    <a href="/discover-ai">Get started</a>
                 </div>
             </main>
         </div>
